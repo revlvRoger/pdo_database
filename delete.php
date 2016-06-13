2f875423e145eb5b5490ba786c4ec795a35969aa
revlvRoger <revlv.roger@gmail.com>
2016-06-13 15:04:18 +0800

    deleting todo list done

--- delete.php
+++ delete.php
@@ -1,21 +1,11 @@
 <?php
-// $dbh = new PDO('odbc:sample', 'db2inst1', 'ibmdb2');
-
-// /* Delete all rows from the FRUIT table */
-// $count = $dbh->exec("DELETE FROM fruit WHERE colour = 'red'");
-
-// /* Return number of rows that were deleted */
-// print("Deleted $count rows.\n");
+$db_connect = new PDO( 'mysql:host=localhost;dbname=todo;charset=utf8mb4', 'root', 'P@ssw0rd' );
+$db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-// $db_connect = new PDO( 'mysql:host=localhost;dbname=todo;charset=utf8mb4', 'root', 'P@ssw0rd' );
-// $db_connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// $statement = $db_connect->query('SELECT * FROM tasks');
-// /* Delete all rows from the FRUIT table */
-// $count = $db_connect->exec("DELETE FROM tasks WHERE id = ");
-
-
-// $sql = "DELETE FROM tasks WHERE id =  :id";
-// $stmt = $db_connect->prepare($sql);
-// $stmt->bindParam(':id', $_POST['id'], PDO::PARAM_INT);
-// $stmt->execute();
+// $event_id=$_GET['id'];
+$result = $db_connect->prepare("DELETE FROM tasks WHERE id= :id");
+$result->bindParam(':id', $_GET['id']);
+$result->execute();
+header("location: pdo.php");
+exit;
